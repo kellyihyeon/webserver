83de4cf3d762ed1a0a1f905811a722ee543267e0
kellykang-tech <kellykang13@gmail.com>
2022-03-02 12:38:07 +0900

    :white_check_mark: Modify: CookieTest

--- src/test/java/com/github/kelly/http/CookieTest.java
+++ src/test/java/com/github/kelly/http/CookieTest.java
@@ -12,18 +12,25 @@ class CookieTest {
     @Test
     @DisplayName("Option 을 설정 하지 않으면 Default Cookie 가 생성된다.")
     void Default_Cookie_객체_생성() {
-        String expectedCookie = "YH_COOKIE_TEST=cookie-value-12345-abcde";
-        Cookie cookie = new Cookie("YH_COOKIE_TEST", "cookie-value-12345-abcde");
+        String cookieName = CookieTypes.YH_COOKIE.name();
+        String cookieValue = "cookie-value-12345-abcde";
+        String expectedCookie = cookieName + "=" + cookieValue;
+
+        Cookie cookie = new Cookie(CookieTypes.YH_COOKIE, cookieValue);
         String bakedCookie = cookie.createCookie();
 
         assertEquals(expectedCookie, bakedCookie);
     }
 
     @Test
-    @DisplayName("Cookie 에 Expires(현재 시간 + 7일) 를 설정한다.")
-    void Optional_Setting한_객체_생성() {
-        String expectedCookie = "YH_COOKIE_TEST=cookie-value-12345-abcde; Expires=Tue, 8 Mar 2022 0:0:0 GMT";
-        Cookie cookie = new Cookie("YH_COOKIE_TEST", "cookie-value-12345-abcde");
+    @DisplayName("Option - Expires 를 설정하면 만료시간이 설정된 Cookie 가 생성된다.")
+    void Optional_Expires_Cookie_객체_생성() {
+        String cookieName = CookieTypes.YH_COOKIE.name();
+        String cookieValue = "cookie-value-12345-abcde";
+        String optionalExpires = "Expires=Tue, 8 Mar 2022 0:0:0 GMT";
+        String expectedCookie = cookieName + "=" + cookieValue +"; " + optionalExpires;
+
+        Cookie cookie = new Cookie(CookieTypes.YH_COOKIE, cookieValue);
         cookie.setExpires(LocalDateTime.of(2022, 3, 1, 0, 0, 0).plusDays(7));
         String bakedCookie = cookie.createCookie();
 
